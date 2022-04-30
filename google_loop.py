70272db177cf8a8cc231b379718a214ac7294334
chewbacca340 <32025365+chewbacca340@users.noreply.github.com>
2022-04-30 08:35:26 -0500

    Update google_loop.py

--- google_loop.py
+++ google_loop.py
@@ -5,10 +5,11 @@ Created on Sun Apr  3 20:28:03 2022
 @author: brayd
 """
 import os
+import requests
 
 api_key = os.environ["gapi"]
 url_start = "https://maps.googleapis.com/maps/api/elevation/json?locations="
-url_end = "=" + api_key
+url_end = "&key=" + api_key
 
 lat = 29.186784154260742
 long = -102.93127864997342
@@ -21,7 +22,7 @@ long_inc = -0.002727835
 
 url_pairs = url_start + ""
 def getUrlPairs(url_pairs):
-    url = url_pairs + url_end
+    url = url_pairs[:-1] + url_end
     print(url)
 
 while lat < final_lat:
@@ -30,9 +31,9 @@ while lat < final_lat:
     while long > final_long:
         long = long + long_inc
          
-        if len(url_pairs + str(lat) + "%2c" + str(long) + url_end) >= 8192-100:
+        if len(url_pairs + str(lat) + "%2C" + str(long) + url_end) >= 300:#8192-100:
             getUrlPairs(url_pairs)
             url_pairs = url_start + ""
 
-        url_pairs = url_pairs + str(lat) + "%2c" + str(long) + "|"
+        url_pairs = url_pairs + str(lat) + "%2C" + str(long) + "|"
   
