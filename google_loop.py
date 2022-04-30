6c665e1aaf1ef2e3373d4e5f10976c82528c8a86
chewbacca340 <32025365+chewbacca340@users.noreply.github.com>
2022-04-29 21:29:30 -0700

    Update google_loop.py

--- google_loop.py
+++ google_loop.py
@@ -17,18 +17,20 @@ final_long = -104.43158817157901
 lat_inc = 0.000958315
 long_inc = -0.002727835
 
-url_pairs = ""
-
+url_pairs = url_start + ""
 def getUrlPairs(url_pairs):
-    print(url_pairs)
+    url = url_pairs + url_end
+    print(url)
 
 while lat < final_lat:
     lat = lat + lat_inc
-    while long < final_long:
+    
+    while long > final_long:
         long = long + long_inc
-        if len(url_start + url_pairs + lat + "%2c" + long + url_end) > 8192:
+         
+        if len(url_pairs + str(lat) + "%2c" + str(long) + url_end) >= 8192-100:
             getUrlPairs(url_pairs)
-            url_pairs = ""
+            url_pairs = url_start + ""
 
-        url_pairs = url_pairs + url_pairs + lat + "%2c" + long + "|"
-            
+        url_pairs = url_pairs + str(lat) + "%2c" + str(long) + "|"
+  
